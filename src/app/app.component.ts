89ab9c2f7ddd551d6bf60f877256da2862859e37
AntonW <awiklund76@gmail.com>
2023-11-02 19:55:51 +0200

    shows navbar evryver else but not on login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'buy-01';
+export class AppComponent implements OnInit {
+  title = 'Buy';
 
+  showNavBar: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Exclude the login component from showing the navbar
+        this.showNavBar = !event.url.includes('/logIn');
+      }
+    });
+  }
 }
