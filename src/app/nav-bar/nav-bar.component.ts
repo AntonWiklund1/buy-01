1a83e0445f0627b27f536d17264d0d14a24a1030
AntonW <awiklund76@gmail.com>
2023-11-02 17:00:26 +0200

    navbar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements  OnInit{
+  constructor() { }
+  showNavBar = false;
+  ngOnInit(): void {
+    this.isLoggedIn();
+    console.log(this.isLoggedIn());
+  }
+  isLoggedIn(){
+    return localStorage.getItem('loggedIn') || false;
+  }
+  logOut(){
+    localStorage.removeItem('loggedIn');
+  }
+  logIn(){
+    localStorage.setItem('loggedIn', 'true');
+  }
 
+  toggleNavBar(){
+    console.log(this.showNavBar);
+    this.showNavBar = !this.showNavBar;
+    return this.showNavBar
+  }
+  showNav(){
+    console.log("show",this.showNavBar);
+    return this.showNavBar
+  }
 }
