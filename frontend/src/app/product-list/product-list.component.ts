78cb6ba678eab5b14c2f606386b6ad57ebc96879
AntonW <awiklund76@gmail.com>
2023-11-21 10:36:05 +0200

    added the right pics for product lists

--- frontend/src/app/product-list/product-list.component.ts
+++ frontend/src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
+import { MediaService } from '../services/media.service';
 
 
 @Component({
@@ -9,8 +10,10 @@ import { ProductService } from '../services/product.service';
 })
 export class ProductListComponent {
   products: any[] | undefined;
+  productMediaUrls: Map<string, string> = new Map(); // Map to store media URLs
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService,    private MediaService: MediaService,
+    ) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(
@@ -21,8 +24,53 @@ export class ProductListComponent {
         console.error(error);
       }
     );
+    this.loadProducts();
 
     console.log("bearer:",localStorage.getItem('bearer'));
+  }
+  loadProducts(): void {
+    this.productService.getProducts().subscribe(
+      (products) => {
+        this.products = products;
+        this.preloadMediaForProducts(products);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  preloadMediaForProducts(products: any[]): void {
+    const backendUrl = 'https://localhost:8443/'; // Adjust this URL to where your backend serves media files
+    products.forEach(product => {
+      this.MediaService.getMedia(product.id).subscribe(
+        (mediaDataArray) => {
+          if (Array.isArray(mediaDataArray) && mediaDataArray.length > 0) {
+            const mediaObject = mediaDataArray[0];
+            if (mediaObject && mediaObject.imagePath) {
+              const imagePath = `${backendUrl}${mediaObject.imagePath}`;
+              this.productMediaUrls.set(product.id, imagePath);
+            } else {
+              console.error(`Media URL not found for product ${product.id}`);
+              this.productMediaUrls.set(product.id, 'https://nayemdevs.com/wp-content/uploads/2020/03/default-product-image.png');
+            }
+          } else {
+            console.error(`Media data is not an array for product ${product.id}`);
+            this.productMediaUrls.set(product.id, 'https://nayemdevs.com/wp-content/uploads/2020/03/default-product-image.png');
+          }
+        },
+        (error) => {
+          console.error(error);
+          this.productMediaUrls.set(product.id, 'https://nayemdevs.com/wp-content/uploads/2020/03/default-product-image.png');
+        }
+      );
+    });
+  }
+  
+    
+
+  getMediaUrl(productId: string): string | undefined {
+    return this.productMediaUrls.get(productId);
   }
 
 }
