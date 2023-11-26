59926aaf99e498e83cabb467a2e935c2e44a2a8f
terujd <jansson.michael.93@gmail.com>
2023-11-26 02:20:24 +0200

    added

---
+++ backend/microservices/product-ms/src/test/java/com/micke/product/ms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.gritlabstudent.media.ms.ms.product.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
