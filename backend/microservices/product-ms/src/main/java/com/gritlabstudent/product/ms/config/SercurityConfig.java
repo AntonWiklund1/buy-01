7c1058104445f5dee2522e97f3c82000548dfcb7
terujd <jansson.michael.93@gmail.com>
2023-11-27 02:05:19 +0200

    fixed imports

--- backend/microservices/product-ms/src/main/java/com/gritlabstudent/product/ms/config/SercurityConfig.java
+++ backend/microservices/product-ms/src/main/java/com/gritlabstudent/product/ms/config/SercurityConfig.java
@@ -1,15 +1,14 @@
 package com.gritlabstudent.product.ms.config;
 
-import com.gritlabstudent.product.ms.filter.JWTFilter;
+import java.util.Arrays;
+import java.util.List;
 
-
-import com.gritlabstudent.media.ms.service.UserInfoDetailsService;
-import jakarta.servlet.Filter;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,21 +17,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import com.gritlabstudent.user.ms.filter.JWTFilter;
+import com.gritlabstudent.user.ms.service.UserInfoDetailsService;
 
-
-import java.util.List;
-import java.util.Arrays;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 
