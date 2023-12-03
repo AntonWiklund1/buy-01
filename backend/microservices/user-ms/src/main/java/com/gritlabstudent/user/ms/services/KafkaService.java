3e3fdf823068b561bc5276ce453888cc17715343
terujd <jansson.michael.93@gmail.com>
2023-12-04 00:52:47 +0200

    added package and imports

--- backend/microservices/user-ms/src/main/java/com/gritlabstudent/user/ms/services/KafkaService.java
+++ backend/microservices/user-ms/src/main/java/com/gritlabstudent/user/ms/services/KafkaService.java
@@ -1,10 +1,18 @@
-//@Service
-//public class KafkaSenderService {
-//
-//    @Autowired
-//    private KafkaTemplate<String, String> kafkaTemplate;
-//
-//    public void sendToTopic(String topic, String payload) {
-//        kafkaTemplate.send(topic, payload);
-//    }
-//}
+package com.gritlabstudent.user.ms.services;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendToTopic(String topic, String payload) {
+        kafkaTemplate.send(topic, payload);
+    }
+}
