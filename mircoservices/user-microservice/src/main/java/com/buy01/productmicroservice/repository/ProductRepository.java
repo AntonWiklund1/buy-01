6736b3ed9ed99273b5d7a88e348a897478fc19b5
AntonW <awiklund76@gmail.com>
2023-12-21 19:56:28 +0200

    Delete unnecessary files and code

--- mircoservices/user-microservice/src/main/java/com/buy01/productmicroservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.buy01.productmicroservice.repository;
-
-import com.buy01.productmicroservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-public interface ProductRepository extends MongoRepository<Product, String>{
-
-
-}
