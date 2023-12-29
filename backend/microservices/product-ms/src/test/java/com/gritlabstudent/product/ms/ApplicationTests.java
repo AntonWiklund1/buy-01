1d1dca2b10edad65a96e07767a509ac77b6cfd65
AntonW <awiklund76@gmail.com>
2023-12-29 21:41:34 +0200

    can mvn clean install

--- backend/microservices/product-ms/src/test/java/com/gritlabstudent/product/ms/ApplicationTests.java
+++ backend/microservices/product-ms/src/test/java/com/gritlabstudent/product/ms/ApplicationTests.java
@@ -3,7 +3,7 @@ package com.gritlabstudent.product.ms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {"spring.kafka.bootstrap-servers=localhost:9092"})
+@SpringBootTest()
 
 class ApplicationTests {
 
--- backend/microservices/user-ms/src/test/java/com/gritlabstudent/user/ms/ApplicationTests.java
+++ backend/microservices/user-ms/src/test/java/com/gritlabstudent/user/ms/ApplicationTests.java
@@ -3,7 +3,7 @@ package com.gritlabstudent.user.ms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {"spring.kafka.bootstrap-servers=localhost:9093"})
+@SpringBootTest()
 
 class ApplicationTests {
 
