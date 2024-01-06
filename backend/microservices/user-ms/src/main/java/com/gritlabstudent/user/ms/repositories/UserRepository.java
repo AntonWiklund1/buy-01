d0fa0898beb20f5425f8708c127438308b93545e
AntonW <awiklund76@gmail.com>
2024-01-06 18:03:23 +0200

    Add findAllEmails method to UserRepository

--- backend/microservices/user-ms/src/main/java/com/gritlabstudent/user/ms/repositories/UserRepository.java
+++ backend/microservices/user-ms/src/main/java/com/gritlabstudent/user/ms/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import com.gritlabstudent.user.ms.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -12,12 +13,9 @@ public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByName(String username);
 
     Optional<User> findByEmail(String email);
-<<<<<<< Updated upstream
-=======
 
     // get all emails from the database
     default List<String> findAllEmails() {
         return findAll().stream().map(User::getEmail).collect(Collectors.toList());
     }
->>>>>>> Stashed changes
 }
